444752c19298cca8fe93182c340aa5f42af01d7d
Topher Brown <t.brown@wordstream.com>
2016-06-29 01:21:14 -0400

    run SQS long polling in a new thread

--- app/newrelic-agent.rb
+++ app/newrelic-agent.rb
@@ -15,23 +15,33 @@ module WordstreamPythonAssertionErrors
     # NewRelic agent setup
     agent_guid "com.wordstream.python-assertion-errors-from-papertrail"
     agent_version "1.0.1"
-    agent_config_options :hertz
-    agent_human_labels("Example Agent") { "Synthetic example data" }
+    agent_human_labels("Python Assertion Errors from Papertrail") { "Asserts" }
+
+    # kick off a thread to populate the local queue with messages from SQS
+    local_queue = Queue.new
+    Thread.new do
+      queue_poller = Aws::SQS::QueuePoller.new(AWS_SQS_URL)
+
+      puts 'set up poller. doing long poll'
+      queue_poller.poll do |msg|
+        puts 'received data from SQS! ', msg.body
+        local_queue.push msg.body
+      end
+    end
 
     def poll_cycle
       puts "running NewRelic poll cycle"
 
-      sqs_client = Aws::SQS::Client.new
-      puts 'getting queues'
-      puts sqs_client.list_queues
-      puts 'got queues'
-      queue_poller = Aws::SQS::QueuePoller.new(AWS_SQS_URL)
-
-      puts 'set up poller. doing poll'
-      queue_poller.poll do |msg|
-        puts 'received data!'
-        puts msg.body
+      # take any messages off the local queue and count them
+      count = 0
+      while not local_queue.empty?
+        puts 'found message in local queue. current count: ', count
+        _ = local_queue.pop
+        count += 1
       end
+
+      # send count to newrelic
+      report_metric "Assertion errors", "count", count
 
       puts "completed NewRelic poll cycle"
     end
