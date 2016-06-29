9ecb96b9efa3c12ae30ef023ba7384547dea6883
Topher Brown <t.brown@wordstream.com>
2016-06-29 11:08:24 -0400

    add kill and logger to start file

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+docker stop nr-plugin
+docker rm nr-plugin
 docker build . -t topher200/sns-to-new-relic
 docker run --env-file .env --name nr-plugin $* topher200/sns-to-new-relic
+docker logs -f nr-plugin
