1d3c1d0569fb2ee95df18f929736d967fb84883d
Topher Brown <topher200@gmail.com>
2016-07-01 01:24:22 -0400

    rename away from 'sns'

--- start-docker.sh
+++ start-docker.sh
@@ -2,6 +2,6 @@
 
 docker stop nr-plugin
 docker rm nr-plugin
-docker build . -t topher200/sns-to-new-relic
-docker run --env-file .env --name nr-plugin $* topher200/sns-to-new-relic
+docker build . -t topher200/sqs-to-new-relic
+docker run --env-file .env --name nr-plugin $* topher200/sqs-to-new-relic
 docker logs -f nr-plugin
